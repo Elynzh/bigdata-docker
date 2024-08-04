0b0a06071db81ea775271daea1959005fcc47e28
appyppah <github@appyppah.dev>
2024-08-04 13:56:12 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,7 @@
+# 大数据集群（docker搭建）
+1. hadoop
+2. hbase
+3. hive
+
+# TODO
+持久化数据
---
+++ hadoop-hbase/conf/hbase-env.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/usr/lib/jvm/jre
+export HBASE_CLASSPATH=/opt/hadoop/etc/hadoop
+export HBASE_DISABLE_HADOOP_CLASSPATH_LOOKUP="true"
